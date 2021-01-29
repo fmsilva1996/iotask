1e9cfee775c209a0e53ae322ddc371db3f41c923
Francisco Silva <fran.silva@thebeansgroup.com>
2021-01-29 15:20:23 +0000

    Add Basic App Theme
    Add typography and colour palatte based on UI kit

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement } from 'react'
-import { ChakraProvider, Heading } from '@chakra-ui/react'
+import { ChakraProvider, Heading, Text } from '@chakra-ui/react'
+import theme from './core/theme'
 
 export default function App(): ReactElement {
   return (
-    <ChakraProvider>
-      <Heading as="h1" size="4xl">
-        Hello, World!
-      </Heading>
+    <ChakraProvider theme={theme}>
+      <Heading textStyle="h1">Hello, World!</Heading>
+      <Text textStyle="textLight">Hello, World!</Text>
     </ChakraProvider>
   )
 }
